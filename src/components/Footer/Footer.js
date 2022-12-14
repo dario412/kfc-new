701bd99a1a558bb2f8c8d744be66495ace67df08
Ani <ani@influencers.club>
2022-12-14 13:24:39 +0100

    footer full

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,16 +6,21 @@ import { styled } from "@mui/material/styles";
 import { lightBlue } from "@mui/material/colors";
 import { ReactComponent as ThreeLines } from "../../assets/3lines.svg";
 import { ReactComponent as FooterLogo } from "../../assets/footer_logo.svg";
+import { ReactComponent as Facebook } from "../../assets/facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/instagram.svg";
+import { ReactComponent as Twitter } from "../../assets/twitter.svg";
+import Typography from "@mui/material/Typography";
 
 const Footer = () => {
   return (
     <Box
       sx={{
         flexGrow: 1,
-
+        boxShadow: 3,
         height: "600px",
         display: "flex",
         justifyContent: "center",
+        p: 0,
       }}
     >
       <Grid container spacing={2} sx={{ width: "1300px" }}>
@@ -24,6 +29,7 @@ const Footer = () => {
           spacing={3}
           sx={{
             height: "20%",
+            mt: 0,
           }}
         >
           <Grid item xs={4}></Grid>{" "}
@@ -34,13 +40,21 @@ const Footer = () => {
               display: "flex",
               justifyContent: "center",
               alignItems: "flex-start",
+              mt: 0,
+              p: 0,
             }}
           >
             <ThreeLines />
           </Grid>
-          <Grid item xs={4}></Grid>
+          <Grid
+            item
+            xs={4}
+            sx={{
+              p: 0,
+            }}
+          ></Grid>
         </Grid>
-        <Grid container spacing={3} sx={{ height: "60%" }}>
+        <Grid container spacing={3} sx={{ height: "60%", p: 0 }}>
           <Grid
             item
             xs={2}
@@ -53,6 +67,9 @@ const Footer = () => {
             }}
             gap={1}
           >
+            <Typography variant="h4" sx={{ color: "black" }}>
+              За Компанијата{" "}
+            </Typography>
             <Link href="#" underline="none" color="black">
               LOREM IPSUM
             </Link>
@@ -78,6 +95,9 @@ const Footer = () => {
             }}
             gap={1}
           >
+            <Typography variant="h4" sx={{ color: "black" }}>
+              За Компанијата{" "}
+            </Typography>
             <Link href="#" underline="none" color="black">
               LOREM IPSUM
             </Link>
@@ -114,6 +134,9 @@ const Footer = () => {
             }}
             gap={1}
           >
+            <Typography variant="h4" sx={{ color: "black" }}>
+              За Компанијата{" "}
+            </Typography>
             <Link href="#" underline="none" color="black">
               LOREM IPSUM
             </Link>
@@ -139,6 +162,10 @@ const Footer = () => {
             }}
             gap={1}
           >
+            {" "}
+            <Typography variant="h4" sx={{ color: "black" }}>
+              За Компанијата{" "}
+            </Typography>
             <Link href="#" underline="none" color="black">
               LOREM IPSUM
             </Link>
@@ -153,12 +180,45 @@ const Footer = () => {
             </Link>
           </Grid>
         </Grid>
-        <Grid
-          container
-          item
-          spacing={3}
-          sx={{ height: "20%", backgroundColor: "#990088" }}
-        ></Grid>
+        <Grid container item spacing={3} sx={{ height: "20%" }}>
+          <Grid
+            item
+            xs={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "start",
+              p: 0,
+            }}
+          >
+            <Link href="#" underline="none" color="black">
+              ® All Rights Reserved{" "}
+            </Link>
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            gap={3}
+          >
+            <Facebook />
+            <Instagram />
+            <Twitter />
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></Grid>
+        </Grid>
       </Grid>
     </Box>
   );
