8f6469893c97d34ac1727eeddd9c146e9cb75f1a
Ani <ani@influencers.club>
2022-12-15 16:38:06 +0100

    ne e celo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import "./App.css";
 import React from "react";
-import Navbar from './components/Navbar/Navbar'
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Pocetna from './components/pages/pocetna/Pocetna';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './theme';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Pocetna from "./components/pages/pocetna/Pocetna";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "./components/pages/meni/Menu";
 import Contact from "./components/pages/pocetna/Contact/contact";
-
+import Kariera from "./components/pages/kariera/Kariera";
 
 function App() {
-  return(
-      <ThemeProvider theme={theme} >
-        <Router>
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
         <Navbar />
-          <Routes>
-            <Route path="/" element={<Pocetna />} />
-            <Route path="/meni" element={<Menu />} />
-            <Route path="/contact" element={<Contact />} />
-
-          </Routes>
-        </Router>
-        <Footer />
-        </ThemeProvider>
-    );
+        <Routes>
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/meni" element={<Menu />} />
+          <Route path="/kontakt" element={<Contact />} />
+          <Route path="/kariera" element={<Kariera />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
 export default App;
