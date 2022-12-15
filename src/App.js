e04d3080aa12b41b5a3bbe1859df68e5c187ff24
Ani <ani@influencers.club>
2022-12-15 13:03:05 +0100

    contact with form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import "./App.css";
 import React from "react";
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer/Footer";
-import Pocetna from './components/pages/pocetna/Pocetna';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './theme';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Pocetna from "./components/pages/pocetna/Pocetna";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./components/pages/pocetna/Contact/contact";
 
 function App() {
-  return(
-      <ThemeProvider theme={theme}>
+  return (
+    <ThemeProvider theme={theme}>
       <Navbar />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Pocetna />} />
-          </Routes>
-        </Router>
-        <Footer />
-        </ThemeProvider>
-    );
+      <Router>
+        <Routes>
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
 export default App;
